ea78f860659db884c96ac33836979865d6cc312a
Yurii <stepanuk.ura2001@gmail.com>
2025-05-20 22:05:17 +0300

    Create database connection, add .env, configuration and validation to .env using Joi

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { DatabaseModule } from './database/database.module';
+import { validationSchema } from './config/validationSchema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+    }),
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ apps/server/src/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigType } from 'src/config/configuration';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<ConfigType>) => ({
+        type: 'postgres',
+        host: configService.get('database.host', { infer: true }),
+        port: configService.get('database.port', { infer: true }),
+        username: configService.get('database.user', { infer: true }),
+        password: configService.get('database.password', { infer: true }),
+        database: configService.get('database.name', { infer: true }),
+        entities: [],
+        synchronize: configService.get('database.sync', { infer: true }),
+      }),
+    }),
+  ],
+  exports: [],
+  providers: [],
+})
+export class DatabaseModule {}
